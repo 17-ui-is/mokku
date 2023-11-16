b0ddb9bdd9cc0fc1fb45dd8c6c6ae8f30c0347df
17-ui-is <xdouxing@gmail.com>
2023-11-16 13:28:56 +0900

    sw

--- js/script.js
+++ js/script.js
@@ -2,6 +2,27 @@ window.addEventListener("scroll", () => {
   const Y = window.scrollY;
   console.log(Y);
   Y >= 1200
-    ? document.querySelector(".X").classList.add("active")
-    : document.querySelector(".X").classList.remove("active");
+    ? document.querySelector(".c1").classList.add("active1")
+    : document.querySelector(".c1").classList.remove("active1");
+});
+window.addEventListener("scroll", () => {
+  const Y = window.scrollY;
+  console.log(Y);
+  Y >= 1350
+    ? document.querySelector(".c2").classList.add("active2")
+    : document.querySelector(".c2").classList.remove("active2");
+});
+window.addEventListener("scroll", () => {
+  const Y = window.scrollY;
+  console.log(Y);
+  Y >= 1500
+    ? document.querySelector(".c3").classList.add("active1")
+    : document.querySelector(".c3").classList.remove("active1");
+});
+window.addEventListener("scroll", () => {
+  const Y = window.scrollY;
+  console.log(Y);
+  Y >= 1700
+    ? document.querySelector(".c4").classList.add("active2")
+    : document.querySelector(".c4").classList.remove("active2");
 });
